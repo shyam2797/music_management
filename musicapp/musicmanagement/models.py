fcf263580e085a335a27272b86ef497ef7db383c
shyam2797 <shyamsaini2797@gmail.com>
2020-09-16 10:22:26 +0000

    Created 2  models

--- musicapp/musicmanagement/models.py
+++ musicapp/musicmanagement/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import Permission, User
 from django.db import models
 
-# Create your models here.
+
+class Album(models.Model):
+    user = models.ForeignKey(User, default=1)
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    song_title = models.CharField(max_length=250)
+    audio_file = models.FileField(default='')
+
+    def __str__(self):
+        return self.song_title
